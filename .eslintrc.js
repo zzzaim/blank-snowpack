39bbc394511e5cc4ea348d1071aab7e609c6f44f
Zaim Bakar <37629946+zzzaim@users.noreply.github.com>
2020-03-08 18:56:29 +0800

    chore: change repo to node-react
    Add react dependencies.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,30 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:prettier/recommended"
+  ],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
   },
   parserOptions: {
-    ecmaVersion: 2018
+    sourceType: "module",
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true
+    }
   },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  plugins: ["react", "react-hooks", "jsx-a11y", "import"],
   ignorePatterns: ["!*rc.js"]
 };
